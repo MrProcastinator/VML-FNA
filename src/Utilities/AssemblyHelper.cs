1ae53e82e4951dce190c7d865b4c61932c59b09a
MrProcastinator <mrprocastinator@tutanota.com>
2025-03-09 12:35:15 -0300

    Remove calls to Assembly.GetEntryAssembly and harcode the name (not used anyway)

--- src/Utilities/AssemblyHelper.cs
+++ src/Utilities/AssemblyHelper.cs
@@ -11,31 +11,31 @@ namespace MonoGame.Utilities
     {
         public static string GetDefaultWindowTitle()
         {
-            // Set the window title.
-            string windowTitle = string.Empty;
+            // // Set the window title.
+            // string windowTitle = string.Empty;
 
-            // When running unit tests this can return null.
-            var assembly = Assembly.GetEntryAssembly();
-            if (assembly != null)
-            {
-                // Use the Title attribute of the Assembly if possible.
-                try
-                {
-                    var assemblyTitleAtt = ((AssemblyTitleAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyTitleAttribute)));
-                    if (assemblyTitleAtt != null)
-                        windowTitle = assemblyTitleAtt.Title;
-                }
-                catch
-                {
-                    // Nope, wasn't possible :/
-                }
+            // // When running unit tests this can return null.
+            // var assembly = Assembly.GetEntryAssembly();
+            // if (assembly != null)
+            // {
+            //     // Use the Title attribute of the Assembly if possible.
+            //     try
+            //     {
+            //         var assemblyTitleAtt = ((AssemblyTitleAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyTitleAttribute)));
+            //         if (assemblyTitleAtt != null)
+            //             windowTitle = assemblyTitleAtt.Title;
+            //     }
+            //     catch
+            //     {
+            //         // Nope, wasn't possible :/
+            //     }
 
-                // Otherwise, fallback to the Name of the assembly.
-                if (string.IsNullOrEmpty(windowTitle))
-                    windowTitle = assembly.GetName().Name;
-            }
+            //     // Otherwise, fallback to the Name of the assembly.
+            //     if (string.IsNullOrEmpty(windowTitle))
+            //         windowTitle = assembly.GetName().Name;
+            // }
 
-            return windowTitle;
+            return "PSVita FNA";
         }
     }
 }
